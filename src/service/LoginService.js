b10832b4a5db8e760ea60e0e99b4a6a7582af80e
vothituongvy <Vothituongvyvttv@gmail.com>
2025-11-09 14:48:12 +0700

    feat(sign in ): Add sign in for website

--- src/service/LoginService.js
+++ src/service/LoginService.js
@@ -1,8 +1,8 @@
 const jwt = require('jsonwebtoken');
 const secretKey = process.env.JWT_SECRET || 'secretkeydefault';
-
+// Xư lý tạo và xác minh token JWT
 const renderToken = (data) => {
-  return jwt.sign(data, secretKey, { expiresIn: '15m' });
+  return jwt.sign(data, secretKey, { expiresIn: '15m' });// Token hết hạn sau 15 phút
 };
 
 const verifyToken = (token) => {
