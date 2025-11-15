2796df54598eb7a4a9c575abd9db273b91f01a44
NguyenKhanhDuy2703 <nguyenkhanhduy270304@gmail.com>
2025-11-15 20:49:41 +0700

    feat : heatmap

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ const dashboardRouter = require("./dashBoard.routes");
 const dwellTimeRouter = require("./dwellTime.routes");
 const cameraZonesRouter = require("./cameraZones.route");
 const dataSynchronizationRouter = require("./dataSynchronization.route");
+const heatmapRouter = require("./heatmap.route");
 const Routes = (app) => {
   app.use("/api/v1/tracking",trackingRouter);
   app.use("/api/v1/downtime", dwellTimeRouter);
   app.use("/api/v1/dashboard", dashboardRouter);
   app.use("/api/v1/cameraZones", cameraZonesRouter);
+  app.use("/api/v1/heatmap", heatmapRouter);
   app.use("/api/v1/dataSynchronization" ,dataSynchronizationRouter );
   app.get("/api", (req, res) => {
     res.send("Welcome to the API");
