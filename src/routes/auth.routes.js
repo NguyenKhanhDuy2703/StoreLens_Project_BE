ac3ec94ce55510c366b92ddd2fd74a56be54465a
NguyenKhanhDuy2703 <nguyenkhanhduy270304@gmail.com>
2025-11-21 18:38:52 +0700

    update : logic authen

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
 const { loginController, registerController, logoutController } = require('../controllers/authController');
-
-// POST /api/v1/auth/login
 router.post('/login', loginController);
 router.post('/register', registerController);
-router.post('/logout', logoutController);
+router.get('/logout', logoutController);
 
 module.exports = router;
