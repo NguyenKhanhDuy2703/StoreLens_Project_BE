e7361e12fd6f1257c1ab1f997c006cac9e7a2f3f
ditordat <161904539+truongthanhdat0406@users.noreply.github.com>
2025-12-03 18:38:23 +0700

    fix: update index.js before pulling

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,8 @@ const dataSynchronizationRouter = require("./dataSynchronization.route");
 const authRouter = require("./auth.routes");
 const heatmapRouter = require("./heatmap.route");
 const storesRouter = require("./stores.routes");
-const { authenticationToken, authenticatioRole } = require("../middlewares/authentication");
+const invoiceRoutes = require('./invoice.routes');
+const { authenticationToken, authenticatioRole  } = require("../middlewares/authentication");
 const Routes = (app) => {
   app.use("/api/v1/tracking",trackingRouter);
   app.use("/api/v1/dashboard", dashboardRouter);
@@ -16,6 +17,7 @@ const Routes = (app) => {
   app.use("/api/v1/dataSynchronization" ,dataSynchronizationRouter );
   app.use("/api/v1/auth", authRouter);
   app.use("/api/v1/stores", storesRouter);
+  app.use("/api/v1/invoices", invoiceRoutes);
   app.get(
     "/api/v1/getToken",
     authenticationToken,
